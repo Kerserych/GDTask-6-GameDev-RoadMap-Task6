77fb8724722341caf43908c5f988eb4e63dbcdfa
Nikita Tsygulev <hekitos007@mail.ru>
2023-04-18 11:53:03 +0300

    Сделал задание

--- Program.cs
+++ Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Вывожу хуйню");
+﻿Console.WriteLine("Введите неприятное приложение");
+Console.WriteLine(antitroll(Console.ReadLine()));
+
+string antitroll(string troll)
+{
+    string nono = "аяуюоеёэиы";
+    return rectroll(troll, nono);
+}
+
+
+string rectroll(string str, string nonono)
+{
+    for (int i = 0; i < str.Length; i++)
+    {
+        if ((nonono.Contains(str[i]) == true) | (nonono.ToUpper().Contains(str[i]) == true))
+        {
+            str = str.Replace($"{str[i]}", "");
+            rectroll(str, nonono);
+        }
+    }
+    return str;
+}
